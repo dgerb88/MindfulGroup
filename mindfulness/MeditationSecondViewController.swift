6cc01c731b01bebe7e36c73d5f71a5a59d9204ad
Dax Gerber <dax.gerber@gmail.com>
2024-04-17 10:10:53 -0600

    Added MeditationSecondViewController and connected in storyboard.

---
+++ mindfulness/MeditationSecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MeditationSecondViewController.swift
+//  mindfulness
+//
+//  Created by Dax Gerber on 4/17/24.
+//
+
+import UIKit
+
+class MeditationSecondViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
